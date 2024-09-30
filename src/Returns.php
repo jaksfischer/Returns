79572391cf9f74b583731596621b8ae7f0866d96
Jakson Fischer <jakson.jfischer@gmail.com>
2024-09-30 14:49:01 -0300

    Add files via upload

---
+++ src/Returns.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @author Jakson Fischer <jakson.fischer@h2grupo.com>
+ * @copyright H2 Grupo
+ * This class is responsible for standardizing returns for the system
+ */
+class Returns
+{
+    /**
+     * @author Jakson Fischer <jakson.fischer@h2grupo.com>
+     * Function to return all data in array
+     * @param bool $status
+     * @param string $message
+     * @param mixed $errorCode
+     * @param mixed $errorType
+     * @param mixed $httpStatus
+     * @return array
+     */
+    public static function toArray(bool $status, string $message, $errorCode = NULL, $errorType = NULL, $httpStatus = NULL)
+    {
+        return [
+            'status'        => $status,
+            'msg'           => $message,
+            'errorCode'     => $errorCode,
+            'errorType'     => $errorType,
+            'httpStatus'    => $httpStatus
+        ];
+    }
+
+    /**
+     * @author Jakson Fischer <jakson.fischer@h2grupo.com>
+     * Function to return data in json
+     * @param bool $status
+     * @param string $message
+     * @param mixed $errorCode
+     * @param mixed $errorType
+     * @param mixed $httpStatus
+     * @return bool|string
+     */
+    public static function toJson(bool $status, string $message, $errorCode = NULL, $errorType = NULL, $httpStatus = NULL)
+    {
+        return json_encode(self::toArray($status, $message, $errorCode, $errorType, $httpStatus));
+    }
+
+    /**
+     * @author Jakson Fischer <jakson.fischer@h2grupo.com>
+     * Function to return in array with data
+     * @param bool $status
+     * @param string $message
+     * @param mixed $errorCode
+     * @param mixed $errorType
+     * @param mixed $httpStatus
+     * @return array[]
+     */
+    public static function arrayDataReturn(bool $status, string $message, $errorCode = NULL, $errorType = NULL, $httpStatus = NULL)
+    {
+        $toArray = self::toArray($status, $message, $errorCode, $errorType, $httpStatus);
+
+        return [
+            'data' => $toArray
+        ];
+    }
+
+    /**
+     * @author Jakson Fischer <jakson.fischer@h2grupo.com>
+     * Function to return in json with data
+     * @param bool $status
+     * @param string $message
+     * @param mixed $errorCode
+     * @param mixed $errorType
+     * @param mixed $httpStatus
+     * @return bool|string
+     */
+    public static function jsonDataReturn(bool $status, string $message, $errorCode = NULL, $errorType = NULL, $httpStatus = NULL)
+    {
+        $toArray = self::toArray($status, $message, $errorCode, $errorType, $httpStatus);
+
+        return json_encode(
+            [
+                'data'=> $toArray
+            ]
+        );
+    }
+}
